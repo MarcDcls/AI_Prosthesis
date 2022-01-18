92e78b9d4be095e2c2376cd8dcad810f69babe0e
DUCLUSAUD Marc <marc.duclusaud@bordeaux-inp.fr>
2022-01-18 15:45:45 +0100

    Correction load_data()

--- data.py
+++ data.py
@@ -13,6 +13,6 @@ def load_data():
     """
     data = np.genfromtxt('data/corpus_students_only_validated_targets.csv', delimiter=',',
                          usecols=(4, 5, 6, 7, 8, 9, 10, 53, 54, 55, 56, 57))[1:, :]
-    inputs = np.concatenate(data[:, :2], data[:, 7:])
+    inputs = np.concatenate((data[:, :2], data[:, 7:]), axis=1)
     outputs = data[:, 2:7]
     return inputs, outputs
