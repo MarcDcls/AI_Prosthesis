3fe007db7843418a38a869047bd2a4a90d2d605c
DUCLUSAUD Marc <marc.duclusaud@bordeaux-inp.fr>
2022-01-18 13:45:37 +0100

    mise en commun

--- data.py
+++ data.py
@@ -1,4 +1,18 @@
-from numpy import genfromtxt
+import numpy as np
 
-my_data = genfromtxt('corpus_students_only_validated_targets.csv', delimiter=',')
-print(my_data[2])
+
+def load_data():
+    """
+    Load data and split them into inputs and outputs with the format :
+
+    inputs = [shPitch, shRoll, handRemapPosX, handRemapPosY, handRemapPosZ, handRemapPitch, handRemapRoll]
+
+    outputs = [armYaw, elbPitch, forearmYaw, wriPitch, wriRoll]
+
+    :return: inputs, outputs
+    """
+    data = np.genfromtxt('data/corpus_students_only_validated_targets.csv', delimiter=',',
+                         usecols=(4, 5, 6, 7, 8, 9, 10, 53, 54, 55, 56, 57))[1:, :]
+    inputs = np.concatenate(data[:, :2], data[:, 7:])
+    outputs = data[:, 2:7]
+    return inputs, outputs
